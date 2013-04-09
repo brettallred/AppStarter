192a075e1c12f093612792721fee8c5f0e685ec2
Leonid Brigadin <leanid.bryhadzin@paralect.com>
2013-04-09 19:47:26 +0300

    Garber Irish has been added

---
+++ src/AppStarter.Web/Assets/vendor/js/garber_irish.js
@@ -0,0 +1,55 @@
+﻿//http://viget.com/inspire/extending-paul-irishs-comprehensive-dom-ready-execution
+
+var SITENAME = {};
+
+var UTIL = {
+    exec: function (controller, action) {
+        var ns = SITENAME,
+            action = (action === undefined) ? "init" : action;
+
+        if (controller !== "" && ns[controller] && typeof ns[controller][action] == "function") {
+            ns[controller][action]();
+        }
+    },
+
+    init: function () {
+        var body = document.body,
+            controller = body.getAttribute("data-controller"),
+            action = body.getAttribute("data-action");
+
+        UTIL.exec("common");
+        UTIL.exec(controller);
+        UTIL.exec(controller, action);
+    },
+
+    // http://stackoverflow.com/questions/9812587/referencing-one-java-script-library-from-another
+
+    loadScript: function (file_name) {
+        if (document.readyState === 'loading') { // Chrome
+            document.write('<script src="' + file_name.replace(/"/g, '&quot;') + '"></script>');
+            return;
+        }
+        var newScript = document.createElement('script');
+        var scripts = document.getElementsByTagName('script');
+
+        // Reference to the latest (this) <script> tag in the document
+        scripts = scripts[scripts.length - 1];
+
+        // Set target
+        newScript.src = file_name;
+
+        // Clean-up code:
+        newScript.onload = newScript.onerror = function () {
+            this.parentNode.removeChild(this);
+        };
+
+        // Insert script in the document, to load it.
+        scripts.parentNode.insertBefore(newScript, scripts);
+    }
+
+};
+
+
+(function ($) {
+    $(document).ready(UTIL.init);
+})(jQuery);
