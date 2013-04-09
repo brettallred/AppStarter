af9dccb80d24194f3f89c5ee9b0c5d2f416851ad
Leonid Brigadin <leanid.bryhadzin@paralect.com>
2013-04-09 15:18:26 +0300

    Page for change informations of current user has been added

---
+++ src/AppStarter.Web/Controllers/UserAccountController.cs
@@ -0,0 +1,44 @@
+﻿using System.Web.Mvc;
+using AppStarter.Models;
+using AppStarter.ViewModels.Account;
+
+namespace AppStarter.Controllers
+{
+    public class UserAccountController : BaseController
+    {
+        //
+        // GET: /UserAccount/
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            var user = Raven.Load<UserAccount>(WebContext.UserAccountId);
+            var model = new UserAccountModel
+            {
+                FullName = user.FullName,
+                Email = user.Email
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(UserAccountModel model)
+        {
+            if (!ModelState.IsValid)
+                return View("Index", model);
+            var user = Raven.Load<UserAccount>(WebContext.UserAccountId);
+            user.Email = model.Email;
+            user.FullName = model.FullName;
+            if(!string.IsNullOrWhiteSpace(model.Password))
+            {
+                user.Password = model.Password;
+            }
+            Raven.Store(user);
+            Raven.SaveChanges();
+            model.Password = string.Empty;
+            MvcFlash.Core.Flash.Success("Information has been saved.");
+            return View("Index", model);
+        }
+
+    }
+}
---
+++ src/AppStarter.Web/ViewModels/Account/UserAccountModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AppStarter.ViewModels.Account
+{
+    public class UserAccountModel
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string FullName { get; set; }
+    }
+}
